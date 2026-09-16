36ad3d94d11df686edbf377cdb6a1b8e1ccff0c2
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-09-16 09:16:22 +0800

    chore: initialize repository

---
+++ packages/utils/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  pack: {
+    deps: { resolveDepSubpath: true },
+    dts: {
+      generator: "tsgo",
+    },
+    exports: true,
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+  fmt: {},
+});
